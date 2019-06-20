58f85046b87468a9c27652b1f1e2b04e9be6ec6d
sivagama <sivagama.srinivasan@sheridanc.on.ca>
2019-06-20 12:25:10 -0400

    Inclass 2 Starter code

---
+++ src/arithmetic/Arithmetic.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arithmetic;
+
+
+import java.util.Scanner;
+
+/** This class calls the method to perform 
+ * arithmetic operations based on user input
+ * execute the code check the output
+ * @author sivagamasrinivasan
+ */
+public class Arithmetic 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+       
+        ArithmeticBase r= new ArithmeticBase();
+        double result = r.calculate(1,2);
+        System.out.println("result :" +result); 
+    
+    }
+
+
+
+
+
+}
+
---
+++ src/arithmetic/ArithmeticBase.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arithmetic;
+
+import java.util.Scanner;
+
+/** This class takes String input plus,minus,divide and times
+ * from user and execute the operation
+ *
+ * @author sivagamasrinivasan
+ */
+public class ArithmeticBase 
+{
+ public double x,y;
+    double calculate(double x, double y) 
+        {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter String");
+        String s= sc.next();
+        switch (s.toUpperCase()) 
+        {
+            case "PLUS":
+                return x + y;
+            case "MINUS":
+                return x - y;
+            case "TIMES":
+                return x * y;
+            case "DIVIDE":
+                return x / y;
+            default:
+                throw new AssertionError("Unknown operations " + this);
+        }
+    }
+   
+}
